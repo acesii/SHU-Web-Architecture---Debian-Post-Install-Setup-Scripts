1fc19b9ee3df6b30314be9b4aaf9a0e46bf4b998
Ian Ibbotson <i.ibbotson@shu.ac.uk>
2011-11-06 14:27:17 +0000

    Added handy script to supress sun java6 license prompt

--- update.bash
+++ update.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # Add the debian non-free repositories to the /etc/apt/sources.list file. The sun JDK comes from the
 # non-free repository, so we add that as a source
 
@@ -18,6 +17,14 @@ echo "deb-src http://security.debian.org/ squeeze/updates main non-free" >> /etc
 # no matching packages for sun-java6-jdk
 
 apt-get update
+
+# there are some great tips for automated installs of the SUN JDK from robert maldons blog, here is the page
+# http://robertmaldon.blogspot.com/2011/08/installing-sun-oracle-jdk-on-ubuntu.html
+# That talks about how to turn off the annoying sun jdk license prompt
+
+echo "Accepting the sun jdk6 license...."
+echo sun-java6-jdk shared/accepted-sun-dlj-v1-1 select true | sudo /usr/bin/debconf-set-selections
+echo sun-java6-jre shared/accepted-sun-dlj-v1-1 select true | sudo /usr/bin/debconf-set-selections
 
 # Use a "Here document" to redirect the text appearing between the !!!s as the standard input
 # for the apt-get command. Essentially saving the user the need to type "Y<enter>"
